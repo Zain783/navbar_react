30a2fec0a3615083025f50e1280ccaebb0ba37de
ZainGillani <gillanizain90@gmail.com>
2023-05-22 20:27:22 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
 import React from 'react';
-import Navbar from './Navbar';
-import UploadCVPage from './uploadcvpage';
 import logoImage from './images/logo.jpg';
+
 function App() {
   return (
     <div>
-      <nav style={{ backgroundColor: 'black', padding: '1rem' }}>
-        <div className="logo">
-        </div>
+      <nav>
+        
         <ul style={{ listStyle: 'none', display: 'flex', gap: '1rem' }}>
-          <li><img src={logoImage} alt="Logo" height={40} width={60} /></li>
           <li>
-            <a href="/" style={{ color: 'white', textDecoration: 'none' }}>
+          <img src={logoImage} alt="Logo" height={40} width={60} />
+          </li>
+          <li>
+            <a href="/" style={{ color: 'black', textDecoration: 'none' }}>
               Home
             </a>
           </li>
           <li>
-            <a href="/about" style={{ color: 'white', textDecoration: 'none' }}>
-              About
+            <a href="/about" style={{ color: 'black', textDecoration: 'none' }}>
+              Product
             </a>
           </li>
           <li>
-            <a href="/services" style={{ color: 'white', textDecoration: 'none' }}>
-              Services
-            </a>
-          </li>
-          <li>
-            <a href="/contact" style={{ color: 'white', textDecoration: 'none' }}>
-              Contact
+            <a href="/services" style={{ color: 'black', textDecoration: 'none' }}>
+             Contact
             </a>
           </li>
         </ul>
       </nav>
-      <div className="upload-cv-page" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <h1 style={{ marginBottom: '1rem' }}>Upload CV</h1>
-        <form className="cv-form" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <input type="file" className="file-input" style={{ marginTop: '1rem' }} />
-          <button type="submit" className="upload-button" style={{ marginTop: '1rem', padding: '0.5rem 1rem', backgroundColor: '#4CAF50', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Upload</button>
+      <div className="upload-cv-page">
+        <h1>Upload Resume</h1>
+        <form className="cv-form">
+          <input type="file" className="file-input" />
+          <button type="submit" className="upload-button">Submit</button>
         </form>
       </div>
-
     </div>
   );
 }
